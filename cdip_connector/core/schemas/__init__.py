e17f5330a73f3740635edfdd03f096a999eb9e87
Mariano M <marianom@earthranger.com>
2023-06-13 19:31:25 -0300

    Move schemas for gundi v2 into a new module

---
+++ cdip_connector/core/schemas/__init__.py
@@ -0,0 +1,2 @@
+from .v1 import *   # Default to v1 for backward compatibility
+from . import v2  # v2 must be explicitly imported
---
+++ cdip_connector/core/schemas/common.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class DestinationTypes(Enum):
+    EarthRanger = "earth_ranger"
+    SmartConnect = "smart_connect"
+    WPSWatch = "wps_watch"
