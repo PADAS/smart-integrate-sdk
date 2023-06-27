d48af8ad6c406ec6f1d9d2b87e888d6a433b5af2
Mariano M <marianom@earthranger.com>
2023-06-27 12:19:01 -0300

    backward compatibility for users importing schemas from cdip connector

--- cdip_connector/core/__init__.py
+++
@@ -1,2 +0,0 @@
-# Backward compatibility for users importing schemas from core
-from gundi_core import schemas
---
+++ cdip_connector/core/schemas/__init__.py
@@ -0,0 +1,2 @@
+# Backward compatibility for users importing schemas from core
+from gundi_core.schemas import *
